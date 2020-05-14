7430c7d331513731c64dafc4cd1e14db74b21388
Joel Fernandes <mail@joelfernandes.net>
2020-05-14 01:04:14 -0400

    Add `update_registrants_status()`

--- zoomus/components/meeting.py
+++ zoomus/components/meeting.py
@@ -80,3 +80,10 @@ class MeetingComponentV2(base.BaseComponent):
     def list_registrants(self, **kwargs):
         util.require_keys(kwargs, "id")
         return self.get_request("/meetings/{}/registrants".format(kwargs.get("id")), params=kwargs)
+
+    def update_registrants_status(self, **kwargs):
+        util.require_keys(kwargs, "id")
+        return self.put_request(
+            "/meetings/{}/registrants/status".format(kwargs.get("id")),
+            params=kwargs
+        )
